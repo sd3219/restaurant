5425a0aa7f3faa8b882154b520a37266f10b9e93
Selly Djap <138817097+sd3219@users.noreply.github.com>
2023-08-10 20:27:03 -0400

    Packaged as function

--- app/restaurant.py
+++ app/restaurant.py
@@ -9,24 +9,39 @@ load_dotenv() #> invoking this function loads contents of the ".env" file into t
 
 API_KEY = os.getenv("API_KEY")
 
-request_url = "https://api.yelp.com/v3/businesses/search"
-request_params = {
+
+def fetch_restaurant_data(zip_code):
+    request_url = "https://api.yelp.com/v3/businesses/search"
+
+    request_params = {
     'term': 'food',
     'limit': 50,
     'offset': 50,
     'radius': 10000,
     'location': 'new york'
-}
-request_headers = {'Authorization': f"bearer {API_KEY}"}
+    }
+    request_headers = {'Authorization': f"bearer {API_KEY}"}
 
-response = requests.get(url=request_url, params=request_params, headers=request_headers)
-restaurant = json.loads(response.text)
-restaurant = restaurant["businesses"]
-restaurant = [ r for r in restaurant if r["rating"] >= 4.5 ]
+    response = requests.get(url=request_url, params=request_params, headers=request_headers)
+    restaurant = json.loads(response.text)
+    restaurant = restaurant["businesses"]
+    restaurant = [ r for r in restaurant if r["rating"] >= 4.5 ]
+
+    return restaurant
 
 
-c_zipcode = input ("What's your zipcode '10001', '10003', '11222' ? ")
 
-for r in restaurant:
-  if c_zipcode == r["location"]["zip_code"]:
-    print (r["name"])
+
+if __name__ == "__main__":
+
+    print("RESTAURANT REPORT...")
+
+    c_zipcode = input ("What's your zipcode '10001', '10003', '11222' ? ")
+    
+    restaurant = fetch_restaurant_data(c_zipcode)
+
+    for r in restaurant:
+        if c_zipcode == r["location"]["zip_code"]:
+            print (r["name"])
+
+
