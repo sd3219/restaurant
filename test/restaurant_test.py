f059216d0a607b21a013d42235c9c36a5d48a612
Selly Djap <138817097+sd3219@users.noreply.github.com>
2023-08-12 15:27:54 -0400

    pytest fixed

--- test/restaurant_test.py
+++ test/restaurant_test.py
@@ -6,25 +6,5 @@ def test_restaurant_data(c_zipcode="10003"):
 
     data = fetch_restaurant_data(c_zipcode)
 
-    # it returns a list of dicts:
+    # it returns a list:
     assert isinstance(data, list)
-    #assert isinstance(data[0], dict)
-
-    # where each has the keys:
-    # "id"
-    # "alias"
-    # "name"
-    # "image_url"
-    # "is_closed"
-    # "url"
-    # "review_count"
-    # "categories"
-    # "rating"
-    # "coordinates"
-    # "transactions"
-    # "price"
-    # "location"
-    # "phone"
-    # "display_phone"
-    # "distance"
-    assert list(data[0].keys()) == ["id", "alias", "name", "image_url", "is_closed", "url", "review_count", "categories", "rating", "coordinates", "transactions", "price", "location", "phone", "display_phone", "distance"]
